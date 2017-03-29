3ac07c4b800a4b82e6277b569c1ad3999d9245aa
Morgan Miller <m.miller722@gmail.com>
2017-03-29 17:33:47 -0600

    adds reference to documentation for usage with postgres db

--- client/index.js
+++ client/index.js
@@ -11,7 +11,9 @@ import SongCreate from './components/SongCreate';
 import SongDetail from './components/SongDetail';
 
 const client = new ApolloClient({
-  dataIdFromObject: o => o.id // this only works with ALL unique ids across DB, not just table (i think)
+  dataIdFromObject: o => o.id
+  // this only works with ALL unique ids across DB, not just table
+  //see below for usage with typical postgres db
 });
 
 const Root = () => {
@@ -32,3 +34,20 @@ ReactDOM.render(
   <Root />,
   document.querySelector('#root')
 );
+
+
+// From Apollo Client Docs: http://dev.apollodata.com/react/cache-updates.html
+// If the IDs are only unique per type (this is typical if an ID is just an
+// ID out of a database table), you can use the `__typename` field to scope it.
+// This is a GraphQL field that's automatically available, but you do need
+// to query for it also. (??? so include __typename in ALL queries ???)
+// const client = new ApolloClient({
+//   dataIdFromObject: (result) => {
+//     if (result.id && result.__typename) {
+//       return result.__typename + result.id;
+//     }
+//
+//     // Make sure to return null if this object doesn't have an ID
+//     return null;
+//   },
+// });

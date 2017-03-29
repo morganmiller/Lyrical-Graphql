84828d13075d4f9b6380aab8373e02365af74602
Morgan Miller <m.miller722@gmail.com>
2017-03-29 12:17:59 -0600

    adds SongCreate mutation

--- client/index.js
+++ client/index.js
@@ -4,8 +4,9 @@ import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
-import App from './components/App'
+import App from './components/App';
 import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
 
 const client = new ApolloClient({});
 
@@ -15,6 +16,7 @@ const Root = () => {
       <Router history={hashHistory}>
         <Route path="/" component={App} >
           <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
         </Route>
       </Router>
     </ApolloProvider>
